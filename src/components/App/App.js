0f164b4f7b38b55c4274ec4a462e2628efff890e
devrok <roland.kurz@gmx.at>
2018-05-06 15:40:08 +0200

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar.js";
+import SearchResults from "../SearchResults/SearchResults.js";
+import Playlist from "../Playlist/Playlist.js";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+// <div>
+//   <h1>Ja<span class="highlight">mmm</span>ing</h1>
+//   <div class="App">
+//     <!-- Add a SearchBar component -->
+//     <div class="App-playlist">
+//     <!-- Add a SearchResults component -->
+//     <!-- Add a Playlist component -->
+//     </div>
+//   </div>
+// </div>
---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./TrackList.css";
+import Track from "../Track/Track.js";
+
+class TrackList extends Component {
+  render() {
+    return (
+      <div className="TrackList">
+      </div>
+    );
+  }
+}
+
+export default TrackList;
+// <div class="TrackList">
+//     <!-- You will add a map method that renders a set of Track components  -->
+// </div>
